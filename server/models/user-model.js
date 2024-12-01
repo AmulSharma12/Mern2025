20a7ce42ea9b0621d6324578448b62baba881733
Amul Op <amulsharmarr67@gmail.com>
2024-12-01 14:50:47 +0530

    Added user model

---
+++ server/models/user-model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+//creating user schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  phone: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+//creaing user model
+const User = new mongoose.model("User", userSchema);
+
+//export User model
+module.exports = User;
