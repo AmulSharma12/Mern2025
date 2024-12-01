48767000dd92778ead0a25c767de2967e2e0a627
Amul Op <amulsharmarr67@gmail.com>
2024-12-01 16:36:40 +0530

    securing password using bcrypt before saving to database - 1 way

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,6 +1,7 @@
 // controller is used to proces incoming request interacting with models(Data source) and sending back the response to client
 //help us to organise the MVC pattern (Model view controller)
 const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
 
 //---------------------**
 //Home controller logic
@@ -27,7 +28,11 @@ const register = async (req, res) => {
       return res.status(400).send({ msg: "user email already exist" });
     }
 
-    const userData = await User.create({ username, email, phone, password });
+    //securing password using bcryp before saving to the database
+    const salt = await bcrypt.genSalt(10);
+    const hashed_password = await bcrypt.hash(password,salt);
+
+    const userData = await User.create({ username, email, phone, password: hashed_password });
     res.status(200).send({ msg: userData });
   } catch (error) {
     res.status(500).send({ msg: "Internal server error" });
