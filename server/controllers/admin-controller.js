1f1234590363fa5a7d0468d85ada479714af4db1
Amul Op <amulsharmarr67@gmail.com>
2024-12-31 00:18:46 +0530

    Admin route - getAllContacts method

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -1,5 +1,5 @@
 const User = require("../models/user-model");
-
+const Contact = require("../models/contact-model");
 const getAllUsers = async (req, res, next) => {
   try {
     //fetching all the users except password field
@@ -11,8 +11,26 @@ const getAllUsers = async (req, res, next) => {
 
     return res.status(200).json(users);
   } catch (error) {
+    console.log(`Admin route - getAllUsers method: ${error}`);
     next(error);
   }
 };
 
-module.exports = getAllUsers;
+//getAllContacts functionality
+const getAllContacts = async (req, res, next) => {
+  try {
+    //fetching all the contacts
+    const contacts = await Contact.find();
+    console.log(contacts);
+    if (!contacts || contacts.length === 0) {
+      return res.status(404).json({ message: "contacts not found" });
+    }
+
+    return res.status(200).json(contacts);
+  } catch (error) {
+    console.log(`Admin route - getAllContacts method: ${error}`);
+    next(error);
+  }
+};
+
+module.exports = { getAllUsers, getAllContacts };
--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const getAllUsers = require("../controllers/admin-controller");
+const {
+  getAllUsers,
+  getAllContacts,
+} = require("../controllers/admin-controller");
 
 //creating routes for admin
 router.route("/users").get(getAllUsers);
+router.route("/contacts").get(getAllContacts);
 
 //exporting the admin router
 module.exports = router;
